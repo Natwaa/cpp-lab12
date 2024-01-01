96ef809b2be3a39a8a50ff8a5918581717cec15d
Natwaa <Natwara_ch@cmu.ac.th>
2024-01-02 00:30:23 +0700

    lab12_4 completed

--- lab12_4.cpp
+++ lab12_4.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+template <typename T>
+void mySwap(T &,T &);
+
 int main(){
 	int x, y;
 	string a, b;
@@ -28,5 +31,12 @@ int main(){
 	cout << "p = " << p << ", q = " << q << "\n";
 
 	return 0;
+}
+
+template <typename currentType>
+void mySwap(currentType &x,currentType &y){
+    currentType z = y;
+    y = x;
+    x = z;
 }
 
